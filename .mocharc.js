56795dec88935f415e0beaf22f54fe10b884a481
YuTengjing <ytj2713151713@gmail.com>
2020-01-16 20:06:01 +0800

    :wrench: configs: disable power-assert when debug

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,12 @@
-module.exports = {
+const configs = {
     recursive: true,
     require: ['intelli-espower-loader'],
     exit: true,
     timeout: 3 * 1000,
 };
+
+if (process.env.DEBUG === '1') {
+    delete configs.require;
+}
+
+module.exports = configs;
